6cdea9cb2ee405810ef4b7aab6913d571e472fa5
Steven Vasquez <s.vasquez85@gmail.com>
2022-01-15 17:03:30 +0900

    Updated destroy method

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,6 +18,8 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
+
+    redirect_to list_path(@bookmark.list)
   end
 
   private
